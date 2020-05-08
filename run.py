90c8b86afcf409b1d81aef92efb49bce3f9e7045
stevenli2020 <stevenli2020@github.com>
2020-05-08 23:23:27 +0100

    compiled version with service key check

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import requests
-import thread
 import sys,os
 import json
 import time
@@ -10,7 +8,7 @@ import signal
 import atexit
 import traceback
 import random
-import hashlib
+import rfid
 
 def HANDLE_EXIT(*arg): 
 	global JOBS,JOB_ID,EXITING,POST_FILE
@@ -38,86 +36,7 @@ def HANDLE_EXIT(*arg):
 atexit.register(HANDLE_EXIT)
 signal.signal(signal.SIGTERM, HANDLE_EXIT)
 signal.signal(signal.SIGINT, HANDLE_EXIT)
-
-def GET_PRESENCE(MODE,URL,INT):
-	global JOB_ID
-	# print "Report mode = "+MODE
-	# print "URL = "+URL
-	while 1:
-		try:
-			R = requests.get(URL)	
-			TIME = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-			if R.status_code != 200:
-				print "Status code:" + str(R.status_code)
-			else: 
-				DATA = json.loads(R.text)
-				try:
-					L = len(DATA['results'])
-					REPORT = {}
-					REPORT['time']=TIME
-					REPORT['type']=MODE					
-					if L > 0:
-						# POST(json.dumps(DATA['results']))
-						if MODE == "location":
-							REPORT['locations']={}
-							for x in range (0,L):
-								LOCATION = DATA['results'][x]['location']
-								ITEM = DATA['results'][x]['item']
-								if LOCATION['id'] not in REPORT['locations']:
-									REPORT['locations'][LOCATION['id']]={}
-								if 'label' not in REPORT['locations'][LOCATION['id']]:
-									REPORT['locations'][LOCATION['id']]['label']=LOCATION['label']
-								if 'items' not in REPORT['locations'][LOCATION['id']]:
-									REPORT['locations'][LOCATION['id']]['items']=[]
-								I = {}
-								I['id']=ITEM['id']
-								I['code_hex']=ITEM['code_hex']
-								I['label']=ITEM['label']
-								I['sets']=ITEM['sets']
-								I['proximity']=DATA['results'][x]['proximity']
-								REPORT['locations'][LOCATION['id']]['items'].append(I)
-						elif MODE == "item" or MODE == "match":
-							REPORT['items']={}
-							for x in range (0,L):
-								LOCATION = DATA['results'][x]['location']
-								ITEM = DATA['results'][x]['item']
-								if ITEM['id'] not in REPORT['items']:
-									REPORT['items'][ITEM['id']]={}
-								if 'label' not in REPORT['items'][ITEM['id']]:
-									REPORT['items'][ITEM['id']]['label']=ITEM['label']
-								if 'locations' not in REPORT['items'][ITEM['id']]:
-									REPORT['items'][ITEM['id']]['locations']=[]	 
-								I = {}	
-								I['id']=LOCATION['id']
-								I['label']=LOCATION['label']
-								I['position']=LOCATION['custom']['position']
-								I['proximity']=DATA['results'][x]['proximity']
-								REPORT['items'][ITEM['id']]['locations'].append(I)
-					print json.dumps(REPORT)+"\n"
-					POST(json.dumps(REPORT))
-				except Exception as e:
-					ERROR = str(e)+"\n"+str(sys.exc_info()[2])
-					LOG(ERROR)
-					pass
-		except Exception as e:
-			ERROR = str(e)+"\n"+str(sys.exc_info()[2])
-			LOG(ERROR)
-			time.sleep(5)
-			continue			
-		time.sleep(INT)
-
-def POST(MESSAGE):
-	global POST_FILE,PID
-	DATA = "PID="+str(PID)+"\n"+datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")+"\r\n"+str(MESSAGE)
-	with open(POST_FILE, 'w+') as f:
-		f.write(DATA)	
-
-def LOG(MESSAGE):
-	global LOG_FILE
-	DATA = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")+"\r\n"+str(MESSAGE)+"\r\n"
-	with open(LOG_FILE, 'a+') as f:
-		f.write(DATA)	
-		
+	
 def UNHANDLED_EXCEPTION(exctype, value, tb):
 	global LOG_FILE
 	MESSAGE = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")+"\r\n<UNHANDLED_EXCEPTION>\n"
@@ -134,7 +53,7 @@ JOB_ID = sys.argv[1]
 POST_FILE = '/app/run/'+JOB_ID
 LOG_FILE = '/app/run/LOG-'+JOB_ID
 PID = os.getpid()
-POST("Job started")
+rfid.POST(PID,POST_FILE,"Job started")
 	
 with open('/app/jobs.json', 'r+') as f0:
 	JOBS = json.loads(f0.read())
@@ -153,37 +72,8 @@ with open('/app/jobs.json', 'r+') as f0:
 	f0.seek(0)
 	f0.write(json.dumps(JOBS))
 	f0.truncate()
-# sys.exit()
-R = requests.get("http://localhost:8080/DeviceId")
 
-while R.status_code != 200:
-	print "Status code:" + str(R.status_code)
-	time.sleep(2)
-	print "Retry ..."
-	R = requests.get("http://localhost:8080/DeviceId")
-DEVICE_ID = json.loads(R.text)['DeviceId']
-# print "DEVICE_ID = "+DEVICE_ID
-SERVICE_KEY = hashlib.sha1(hashlib.sha512(DEVICE_ID).hexdigest()+hashlib.sha512("AVNET").hexdigest()).hexdigest()
-# print "SERVICE_KEY = "+SERVICE_KEY
-if SERVICE_KEY != JOBS[JOB_ID]['config']['service_key']:
-	print "Service key error"
-	sys.exit()
-
-QUERYSTRING = ""
-if 'items' in JOBS[JOB_ID]:
-	for item in JOBS[JOB_ID]['items']:
-		QUERYSTRING = QUERYSTRING + "&item=" + item
-if 'locations' in JOBS[JOB_ID]:		
-	for location in JOBS[JOB_ID]['locations']:
-		QUERYSTRING = QUERYSTRING + "&location=" + location
-print "Start job now"
-print QUERYSTRING
-
-thread.start_new_thread(GET_PRESENCE,(JOBS[JOB_ID]['mode'],"https://"+JOBS[JOB_ID]['config']['brain_address']+"/api/presences?limit=1000&key="+JOBS[JOB_ID]['config']['api_key']+"&populate=item,location"+QUERYSTRING,JOBS[JOB_ID]['config']['report_interval']))
-
-while 1:
-	time.sleep(10)
-
+rfid.GET_PRESENCE(JOBS,JOB_ID,POST_FILE,LOG_FILE)
 		
 		 	
 
